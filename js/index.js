603d092577edf19b12551c1ce213625417aeccd1
Bruno-VILLALON <brunovillalon@gmail.com>
2023-01-26 14:26:47 +0100

    add window.onload function

--- js/index.js
+++ js/index.js
@@ -1,35 +1,37 @@
-// locomotivescroll inisialization
+window.onload = () => {
 
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true,
-    tablet: {smooth: true},
-    smartphone: {smooth: true}
-})
+    // locomotivescroll inisialization
+
+    const scroll = new LocomotiveScroll({
+        el: document.querySelector('[data-scroll-container]'),
+        smooth: true,
+        tablet: {smooth: true},
+        smartphone: {smooth: true}
+    })
 
 
-// body color
+    // body color
 
-const darkColor = "#2d3e4e";
-const mediumColor = "#8cbdb9";
-const lightColor = "#e8eceb";
-const contrastColor = "#e09e50";
+    const darkColor = "#2d3e4e";
+    const mediumColor = "#8cbdb9";
+    const lightColor = "#e8eceb";
+    const contrastColor = "#e09e50";
 
-scroll.on("scroll", () => {
-    if (document.querySelector("#header-color.is-inview")) {
-        document.body.style.background = darkColor;
-        document.body.style.color = lightColor;
-        //console.log("écran d'accueil");
-    } else if (document.querySelector("#skills-color.is-inview")){
-        document.body.style.background = mediumColor;
-        document.body.style.color = darkColor;
-        //console.log("section compétences")
-    } else {
-        document.body.style.background = lightColor;
-        document.body.style.color = darkColor;
-    }
-})
-
+    scroll.on("scroll", () => {
+        if (document.querySelector("#header-color.is-inview")) {
+            document.body.style.background = darkColor;
+            document.body.style.color = lightColor;
+            //console.log("écran d'accueil");
+        } else if (document.querySelector("#skills-color.is-inview")){
+            document.body.style.background = mediumColor;
+            document.body.style.color = darkColor;
+            //console.log("section compétences")
+        } else {
+            document.body.style.background = lightColor;
+            document.body.style.color = darkColor;
+        }
+    })
+}
 
 // EmailJS
 
